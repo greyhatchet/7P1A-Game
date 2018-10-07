36594bf74c2dd704f7deeda85b3e4c6d7b65d66f
Tyson-Smiter <34221646+Tyson-Smiter@users.noreply.github.com>
2018-10-07 15:34:29 -0500

    The R button now restarts users to the beginning of any level!

--- Platform.py
+++ Platform.py
@@ -31,34 +31,6 @@ def message_to_screen(msg,color,x_displace=0, y_displace =0):
 global look_forward
 look_forward = True
 
-class SpriteSheet(object):
-    """ Class used to grab images out of a sprite sheet. """
- 
-    def __init__(self, file_name):
-        """ Constructor. Pass in the file name of the sprite sheet. """
- 
-        # Load the sprite sheet.
-        self.sprite_sheet = pygame.image.load(file_name).convert()
- 
- 
-    def get_image(self, x, y, width, height):
-        """ Grab a single image out of a larger spritesheet
-            Pass in the x, y location of the sprite
-            and the width and height of the sprite. """
- 
-        # Create a new blank image
-        image = pygame.Surface([width, height]).convert()
- 
-        # Copy the sprite from the large sheet onto the smaller image
-        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
- 
-        # Assuming black works as the transparent color
-        image.set_colorkey(BLACK)
- 
-        # Return the image
-        return image
-
-
 class Player(pygame.sprite.Sprite):
 
     #This class represents the bar at the bottom that the player controls.
@@ -82,48 +54,8 @@ class Player(pygame.sprite.Sprite):
         self.change_x = 0
         self.change_y = 0
 
-        # This holds all the images for the animated walk left/right
-        # of our player
-        self.walking_frames_l = []
-        self.walking_frames_r = []
- 
-        # What direction is the player facing?
-        self.direction = "R"
-
         # List of sprites we can bump against
         self.level = None
-
-        sprite_sheet = SpriteSheet("boy.png")
-        # Load all the right facing images into a list
-        image = sprite_sheet.get_image(0, 0, 65, 60)
-        self.walking_frames_r.append(image)
-        image = sprite_sheet.get_image(65, 0, 65, 60)
-        self.walking_frames_r.append(image)
-        image = sprite_sheet.get_image(130, 0, 65, 60)
-        self.walking_frames_r.append(image)
-        image = sprite_sheet.get_image(195, 0, 65, 60)
-        self.walking_frames_r.append(image)
- 
-        # Load all the right facing images, then flip them
-        # to face left.
-        image = sprite_sheet.get_image(0, 0, 65, 60)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_l.append(image)
-        image = sprite_sheet.get_image(65, 0, 65, 60)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_l.append(image)
-        image = sprite_sheet.get_image(130, 0, 65, 60)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_l.append(image)
-        image = sprite_sheet.get_image(195, 0, 65, 60)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_l.append(image)
- 
-        # Set the image the player starts with
-        self.image = self.walking_frames_r[0]
- 
-        # Set a reference to the image rect.
-        self.rect = self.image.get_rect()
 
 
     def update(self):
@@ -133,13 +65,6 @@ class Player(pygame.sprite.Sprite):
 
         # Move left/right
         self.rect.x += self.change_x
-        pos = self.rect.x + self.level.world_shift
-        if self.direction == "R":
-            frame = (pos // 30) % len(self.walking_frames_r)
-            self.image = self.walking_frames_r[frame]
-        else:
-            frame = (pos // 30) % len(self.walking_frames_l)
-            self.image = self.walking_frames_l[frame]
 
         # See if we hit anything
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
@@ -198,13 +123,11 @@ class Player(pygame.sprite.Sprite):
     def go_left(self):
         # Called when the user hits the left arrow.
         self.change_x = -6
-        self.direction = "L"
         look_forward = False
 
     def go_right(self):
         # Called when the user hits the right arrow.
         self.change_x = 6
-        self.direction = "R"
         look_forward = True
 
     def stop(self):
@@ -386,17 +309,18 @@ def main():
     player.level = current_level
 
     player.rect.x = 340
+    position_scroll = 0
     player.rect.y = SCREEN_HEIGHT - player.rect.height
     active_sprite_list.add(player)
 
     # Loop until the user clicks the close button.
     done = False
-
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
 
     # -------- Main Program Loop -----------
     while not done:
+        restart_level = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -412,6 +336,8 @@ def main():
                     shoot =  Shoot()
                     shoot.rect.x
                     shoot.fire()
+                if event.key == pygame.K_r:
+                    restart_level = True
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT and player.change_x < 0:
@@ -429,23 +355,33 @@ def main():
         if player.rect.right >= 500:
             diff = player.rect.right - 500
             player.rect.right = 500
+            position_scroll+= diff
             current_level.shift_world(-diff)
 
         # If the player gets near the left side, shift the world right (+x)
         if player.rect.left <= 120:
             diff = 120 - player.rect.left
             player.rect.left = 120
+            position_scroll -= diff
             current_level.shift_world(diff)
+
+        if restart_level == True:
+            if position_scroll != 0:
+                current_level.shift_world(position_scroll)
+                position_scroll = 0
+                player.rect.x = 120
+
 
         # If the player gets to the end of the level, go to the next level, if at end of last level, print you win
         mScreen = False
         current_position = player.rect.x + current_level.world_shift
         if current_position < current_level.level_limit:
-            player.rect.x = 120
             if current_level_no < len(level_list) - 1:
+                player.rect.x = 120
                 current_level_no += 1
                 current_level = level_list[current_level_no]
                 player.level = current_level
+                position_scroll = 0
             else:
                 mScreen = True
             
