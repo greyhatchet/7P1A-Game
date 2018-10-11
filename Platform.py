cb630618528c85b04d5e32348bf23c302febd0b6
aylwg <43182190+aylwg@users.noreply.github.com>
2018-10-11 11:13:31 -0500

    Update Platform.py

--- Platform.py
+++ Platform.py
@@ -140,6 +140,33 @@ def message_to_screen(msg, color, x_displace=0, y_displace=0, font_size=0):
     screen.blit(textSurf, textRect)
 
 
+class SpriteSheet(object):
+    """ Class used to grab images out of a sprite sheet. """
+ 
+    def __init__(self, file_name):
+        """ Constructor. Pass in the file name of the sprite sheet. """
+ 
+        # Load the sprite sheet.
+        self.sprite_sheet = pygame.image.load(file_name).convert()
+ 
+ 
+    def get_image(self, x, y, width, height):
+        """ Grab a single image out of a larger spritesheet
+            Pass in the x, y location of the sprite
+            and the width and height of the sprite. """
+ 
+        # Create a new blank image
+        image = pygame.Surface([width, height]).convert()
+ 
+        # Copy the sprite from the large sheet onto the smaller image
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+ 
+        # Assuming black works as the transparent color
+        image.set_colorkey(BLACK)
+ 
+        # Return the image
+        return image
+
 global look_forward
 look_forward = True
 
@@ -167,8 +194,49 @@ class Player(pygame.sprite.Sprite):
         self.change_x = 0
         self.change_y = 0
 
+                # This holds all the images for the animated walk left/right
+        # of our player
+        self.walking_frames_l = []
+        self.walking_frames_r = []
+ 
+        # What direction is the player facing?
+        self.direction = "R"
+
         # List of sprites we can bump against
         self.level = None
+
+        sprite_sheet = SpriteSheet("boy.png")
+        # Load all the right facing images into a list
+        image = sprite_sheet.get_image(0, 0, 65, 60)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(65, 0, 65, 60)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(130, 0, 65, 60)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(195, 0, 65, 60)
+        self.walking_frames_r.append(image)
+ 
+        # Load all the right facing images, then flip them
+        # to face left.
+        image = sprite_sheet.get_image(0, 0, 65, 60)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(65, 0, 65, 60)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(130, 0, 65, 60)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(195, 0, 65, 60)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+ 
+        # Set the image the player starts with
+        self.image = self.walking_frames_r[0]
+ 
+        # Set a reference to the image rect.
+        self.rect = self.image.get_rect()
+        
 
     def update(self):
         # Move the player.
@@ -177,6 +245,13 @@ class Player(pygame.sprite.Sprite):
 
         # Move left/right
         self.rect.x += self.change_x
+        pos = self.rect.x + self.level.world_shift
+        if self.direction == "R":
+            frame = (pos // 30) % len(self.walking_frames_r)
+            self.image = self.walking_frames_r[frame]
+        else:
+            frame = (pos // 30) % len(self.walking_frames_l)
+            self.image = self.walking_frames_l[frame]
 
         # See if we hit anything
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
